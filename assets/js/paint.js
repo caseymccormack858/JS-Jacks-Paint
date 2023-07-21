b1053155ee9f9ab531f16a670cef91345e75a30f
Casey McCormack <caseymccormack858@gmail.com>
2023-07-21 04:09:23 -0700

    updated price function

--- assets/js/paint.js
+++ assets/js/paint.js
@@ -39,38 +39,47 @@ function getProductInfo(paintColor) {
         case 'pn1':
             colorName = 'Lime Green';
             price = 14.99;
+            updatePrice(colorName, price)
             break;
           case 'pn2':
             colorName = 'Medium Brown';
             price = 11.14;
+            updatePrice(colorName, price)
             break;
           case 'pn3':
             colorName = 'Royal Blue';
             price = 22.99;
+            updatePrice(colorName, price)
             break;
           case 'pn4':
             colorName = 'Solid Black';
             price = 4.99;
+            updatePrice(colorName, price)
             break;
           case 'pn5':
             colorName = 'Solid Cyan';
             price = 8.22;
+            updatePrice(colorName, price)
             break;
           case 'pn6':
             colorName = 'Solid Purple';
             price = 11.99;
+            updatePrice(colorName, price)
             break;
           case 'pn7':
             colorName = 'Solid Red';
             price = 13.42;
+            updatePrice(colorName, price)
             break;
           case 'pn8':
             colorName = 'Solid White';
             price = 21.98;
+            updatePrice(colorName, price)
             break;
           case 'pn9':
             colorName = 'Solid Yellow';
             price = 14.99;
+            updatePrice(colorName, price)
             break;
           default:
     }
